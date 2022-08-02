2d6e2a61e83f9e2ff765512a644e1b50d30afc5a
Tariel Morita <tariel@jeraapps.com>
2022-08-02 12:03:14 -0300

    initial commit

---
+++ app/src/main/java/com/morita/jera_movie_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.morita.jera_movie_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
